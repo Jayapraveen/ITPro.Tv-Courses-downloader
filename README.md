42b42f838caaa6255515c244c93cd1a0921b5f64
Jayapraveen <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2021-03-21 23:56:44 +0530

    Add downloading support and configuration file
    The script will now support downloading courses and a seperate configuration file is added.

--- README.md
+++ README.md
@@ -5,15 +5,14 @@
 2. Login to the ITPro.tv site on a web browser after opening the network tab on the developer tools
 3. Click dashboard and search for "passedToken" and copy the token starting with "eyJ" completely to clipboard
 ![Token Instructions](https://i.imgur.com/gCrSSnQ.png)
-4. Open the script in an text editor of your choice.
-5. paste the token by replacing the "eyJxxxx.eyJxxxxx0" in login_data string
-6. Run the script.
+4. Open the itProDownloaderConfig.json in an text editor of your choice.
+5. paste the token by replacing the "eyJxxxx.eyJxxxxx0" in token string
+6. Run the script using python. eg. python3 itprotv.py
 7. It should list all the Categories in the site.
 8. Enter the number corresponding to the title.
-9. Wait till the script finishes.
-10. You should find a "download_links.txt" file on the same directory where the script is located.
-11. open the text file.
-12. Use any downloading softwares such as IDM to download the videos files.
+9. Wait till the script finishes downloading.
+##NOTE: 
+To get the download links instead of downloading the videos, change the method in the config file to "list". "download_links.txt" file on the same directory where the script is located will appear having the download links of all the videos files. Use any downloading softwares such as IDM/Aria2 to download the videos files.
 
 ## Usage Disclaimer
 1. Intended for Educational purposes only.
--- itprotv.py
+++ itprotv.py
@@ -4,35 +4,30 @@ Date : 3/19/2021
 Place : India
 Author : Jayapraveen AR
 Program aim : To generate downloadable video links at ITProtv website
-Version : 1.0.2
+Version : 1.0.3
+ToDo:
+1.Download Attachments
+2.Download subtitles
 """
-import requests,json
-
-#Loggedin Token
-login_data = "eyJxxxxxx.eyJxxxxxxxxo"
+import requests,json,os,shutil
 
 #Endpoints
+tags_url = "https://api.itpro.tv/api/mobile/v1/tag_categories/brand/ITProTV"
 iter_cert_course_url = "https://api.itpro.tv/api/mobile/v1/brand/ITProTV/tag_categories/certification/"
 iter_category_course_url = "https://api.itpro.tv/api/mobile/v1/brand/ITProTV/tag_categories/course-library/"
-login_url = "https://api.itpro.tv/api/urza/v3/mobile/android/login"
+iter_course_url = "https://api.itpro.tv/api/urza/v3/consumer-web/course?url="
 #Device details
 brand = "ITProTV"
 brand_itpro = "00002560-0000-3fa9-0000-1d61000035f3"
 ua = "ItProTvApp/2.3.7 (7) (Ios 13; en)"
 
+resolution_data = {"1080p": "jwVideo1080Embed", "720p": "jwVideo720Embed", "480p": "jwVideo480Embed", "320p": "jwVideo320Embed"}
 def list_categories(course_tags):
     categories = []
     for index,course in enumerate(course_tags):
-        print("{0:} {1:}".format(index,course['name']))
-        categories.append(course['name'])
+        print("{0:} {1:}".format(index,course["name"]))
+        categories.append(course["name"])
     return categories
-
-def list_iter_categories(iter_course):
-    iter_categories = []
-    for index,course in enumerate(iter_course):
-        print("{0:} {1:}".format(index,course['name']))
-        iter_categories.append(course['name'])
-    return iter_categories
 
 def get_episodes(episodes):
     episodes_url = []
@@ -41,6 +36,46 @@ def get_episodes(episodes):
         episodes_url.append(value['url'])
     print("Total= "+str(len(episodes_url)))
     return episodes_url
+
+def download_video(url,filename,epoch = 0):
+    if(os.path.isfile(filename)):
+        video_head = requests.head(url, allow_redirects = True)
+        if video_head.status_code == 200:
+            video_length = int(video_head.headers.get("content-length"))
+            if(os.path.getsize(filename) >= video_length):
+                print("Video already downloaded.. skipping Downloading..")
+            else:
+                print("Redownloading faulty download..")
+                os.remove(filename) #Improve removing logic
+                download_video(url,filename)
+        else:
+            if (epoch > retry):
+                exit("Server doesn't support HEAD.")
+            download_video(url,filename,epoch + 1)
+    else:
+        video = requests.get(url, stream=True)
+        video_length = int(video.headers.get("content-length"))
+        if video.status_code == 200:
+            if(os.path.isfile(filename) and os.path.getsize(filename) >= video_length):
+                print("Video already downloaded.. skipping write to disk..")
+            else:
+                try:
+                    with open(filename, 'wb') as video_file:
+                        shutil.copyfileobj(video.raw, video_file)
+                except:
+                    print("Connection error: Reattempting download of video..")
+                    download_video(url,filename, epoch + 1)
+
+            if os.path.getsize(filename) >= video_length:
+                pass
+            else:
+                print("Error downloaded video is faulty.. Retrying to download")
+                download_video(url,filename, epoch + 1)
+        else:
+            if (epoch > retry):
+                exit("Error Video fetching exceeded retry times.")
+            print("Error fetching video file.. Retrying to download")
+            download_video(url,filename, epoch + 1)
 
 def get_link(episodes):
     links = []
@@ -55,37 +90,59 @@ def get_link(episodes):
         print("Error link does not exist")
     return links
 
-def get_download_links(episode_links,data_auth):
-    download_links = []
+def get_download_links(episode_links,data_auth,resolution):
+    download_links = {}
     for i in episode_links:
         req_data = requests.get(url = i, headers = data_auth)
         req_data = json.loads(req_data.text)
-        print(req_data)
-        download_links.append(req_data['episode']['jwVideo1080Embed'])
-    print(download_links)
+        if (req_data["episode"][resolution] == None):
+            resolution = next_resolution
+        download_links[req_data["episode"]["title"]] = req_data["episode"][resolution] # Add the episode url corresponding to the resolution selected
     return download_links
 
+if os.path.isfile("itprodownloader.json"):
+    with open("itprodownloader.json",'r') as config:
+        config_data = config.read()
+    config_data = json.loads(config_data)
+else:
+    exit("Configuration File is missing..\nCheck out Readme.md to find Instructions.")
 
-data_auth = {"Authorization":"Bearer "+login_data,"user-agent":ua}
-course_data = requests.get('https://api.itpro.tv/api/mobile/v1/tag_categories/brand/ITProTV')
+session_token = config_data.get("token",None)
+download_location = config_data.get("downloadLocation",".")
+os.chdir(download_location)
+if(len(session_token) != 865):
+    exit("Token entered is not valid!")
+resolution = config_data.get("downloadQuality",None)
+next_resolution = resolution_data.get(str(int(resolution.split('p')[0])-360) + 'p')
+resolution = resolution_data.get(resolution,None)
+exit("Download Quality entered is invalid..") if(resolution == None) else 1
+method = config_data.get("method","download")
+data_auth = {"Authorization":"Bearer "+session_token,"user-agent":ua}
+course_data = requests.get(tags_url)
 course_data = json.loads(course_data.text)
 course_library_url = course_data['tagCategories'][0]['url']
 course_tags = course_data['tagCategories'][0]['tags']
 categories = list_categories(course_tags)
 choice = int(input("\nEnter your choice: "))
-iter_course = iter_category_course_url + course_tags[choice]['url'] + "/courses"
+iter_course = iter_category_course_url + course_tags[choice]["url"] + "/courses"
 iter_course_data = requests.get(iter_course)
 iter_course_data = json.loads(iter_course_data.text)
-iter_course = list_iter_categories(iter_course_data['courses'])
+iter_course = list_categories(iter_course_data['courses'])
 choice = int(input("\nEnter your choice: "))
-iter_course_data_url = "https://api.itpro.tv/api/urza/v3/consumer-web/course?url="+ iter_course_data['courses'][choice]['url'] +"&brand=" + brand_itpro
+iter_course_data_url = iter_course_url + iter_course_data['courses'][choice]['url'] +"&brand=" + brand_itpro
 episodes = requests.get(iter_course_data_url,headers = data_auth)
 episodes = json.loads(episodes.text)
 episodes = get_episodes(episodes["course"]["episodes"])
 episode_links = get_link(episodes)
-episodes_download_links = get_download_links(episode_links,data_auth)
-writer = open("download_links.txt",'a+')
-for i in episodes_download_links:
-    writer.write(str(i))
-    writer.write("\n")
-writer.close()
+episodes_download_links = get_download_links(episode_links,data_auth,resolution)
+if(method == "download"):
+    for index,data in enumerate(episodes_download_links.items()):
+        filename = str(index) + '.' +data[0] + ".mp4"
+        url = data[1]
+        download_video(url,filename)
+else:
+    writer = open("download_links.txt",'a+')
+    for key,value in episodes_download_links:
+        writer.write(value)
+        writer.write("\n")
+    writer.close()
